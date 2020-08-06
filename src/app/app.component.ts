52632f631e4530805365265e497387f0b2575122
unknown <103043833@student.swin.edu.au>
2020-08-06 20:03:25 +1000

    new test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'myapp',
+  template: `<h1>where are you work hard please
+  </h1>
+  <button (click)="onclick()">mybutton</button>
+  <h2>count up {{counter}}</h2>
+  <div>{{title}}</div>
+  {{title.toUpperCase()}}
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  counter=0;
+  onclick(){
+    this.counter++;
+if(this.counter%5==0){
+  this.title="mmm"
+}
+else if(this.counter%3==0){
+  this.title="ddd"
+}
+else{
+  this.title="boring"
+}
+  }
 }
